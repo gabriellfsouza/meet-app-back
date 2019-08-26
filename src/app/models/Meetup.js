79edba58ad48910730ff71877c69e5c6d64c73dc
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-26 06:34:46 -0300

    chore: meetup migration and module included

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,40 @@
+import Sequelize, { Model } from 'sequelize';
+import { isBefore } from 'date-fns';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.File, { foreignKey: 'banner_id', as: 'banner' });
+  }
+
+  /**
+   * Verifica se a data enviada é maior que a data atual.
+   * @param {Date} date
+   * @returns {Boolean}
+   */
+  checkDate(date) {
+    return date.getTime() > new Date().getTime();
+  }
+}
+
+export default Meetup;
