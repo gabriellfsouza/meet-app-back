32b2eabe3557fbc4405c5b92f6468e77a37588ff
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-26 05:16:23 -0300

    chore: status code changed when password does not match

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -63,7 +63,7 @@ class UserController {
     }
 
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({ error: 'Password does not match' });
+      return res.status(400).json({ error: 'Password does not match' });
     }
 
     const { id, name } = await user.update(req.body);
